089702f18fbf552911981c00c2799e59bcc53bf1
Murillo Bittencourt <murillobittencourt@gmail.com>
2023-10-28 17:53:25 -0300

    Add tailwind support

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 
 import HelloWorld from '../hello-world.vue'
+import Cart from '../cart.vue'
 
 createApp(HelloWorld).mount('#hello-world')
+createApp(Cart).mount('#cart')
